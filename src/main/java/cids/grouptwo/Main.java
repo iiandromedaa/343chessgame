0cbd88c751ae405480290d94a0605023e26f9c92
andro <90076821+iiandromedaa@users.noreply.github.com>
2025-02-20 14:13:40 -0600

    fixed boilerplate lol
    test
    
    Revert "test"
    
    This reverts commit 2ff2aa50113092f864b79d1cc289e6ef8484f8cb.

--- src/main/java/cids/grouptwo/Main.java
+++ src/main/java/cids/grouptwo/Main.java
@@ -4,7 +4,7 @@ public class Main {
     
     public static void main(String[] args) {
         // uncomment this to launch the default GUI that came with the template
-        // FXAppBoot.launch();
+        // FXAppBoot.run();
         System.out.println("hi world hello world hii!!!!!");
     }
 
