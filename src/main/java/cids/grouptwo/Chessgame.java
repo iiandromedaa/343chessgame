afe77b84330fc0700b404c35d65d2fc3cb418c69
andro <90076821+iiandromedaa@users.noreply.github.com>
2025-02-28 16:12:38 -0600

    forgot some stuff

--- src/main/java/cids/grouptwo/Chessgame.java
+++ src/main/java/cids/grouptwo/Chessgame.java
@@ -8,35 +8,28 @@ public class Chessgame implements ApplicationListener {
 
     ShapeRenderer shape;
 
-    @Override
     public void create() {
         shape = new ShapeRenderer();
     }
 
-    @Override
     public void render() {
         shape.begin(ShapeRenderer.ShapeType.Filled);
         shape.circle(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, 50);
         shape.end();
     }
 
-    @Override
     public void dispose() {
         // TODO Auto-generated method stub
     }
 
-    @Override
     public void pause() {
         // TODO Auto-generated method stub
     }
 
-    @Override
     public void resize(int arg0, int arg1) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'resize'");
     }
 
-    @Override
     public void resume() {
         // TODO Auto-generated method stub
     }
