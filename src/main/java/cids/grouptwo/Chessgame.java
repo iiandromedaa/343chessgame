45e131b51f4dd3a389455333b2ebb9e9925c9399
andro <90076821+iiandromedaa@users.noreply.github.com>
2025-02-28 13:38:34 -0600

    switched application class implementation

--- src/main/java/cids/grouptwo/Chessgame.java
+++ src/main/java/cids/grouptwo/Chessgame.java
@@ -1,10 +1,10 @@
 package cids.grouptwo;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
-public class Chessgame extends ApplicationAdapter {
+public class Chessgame implements ApplicationListener {
 
     ShapeRenderer shape;
 
@@ -18,6 +18,27 @@ public class Chessgame extends ApplicationAdapter {
         shape.begin(ShapeRenderer.ShapeType.Filled);
         shape.circle(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, 50);
         shape.end();
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void resize(int arg0, int arg1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resize'");
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
     }
 
 }
