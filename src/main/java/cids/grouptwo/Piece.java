1ca9d0a98dade384c066e1211ffe3c7d59f3df44
GavinSchnepper <grschnepper@gmail.com>
2025-03-27 14:58:07 -0500

    COMMIT_EDITMSG made a branch

---
+++ src/main/java/cids/grouptwo/Piece.java
@@ -0,0 +1,31 @@
+package cids.grouptwo;
+
+public abstract class Piece {
+    private int x, y;
+    private String color;
+
+    public Piece(String color, int x, int y){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void piecePosition(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
