8f37f8db29c66ca88e874011153538e3c824a8d9
andro <90076821+iiandromedaa@users.noreply.github.com>
2025-04-01 20:05:53 -0500

    merge part 2

--- src/main/java/cids/grouptwo/Coordinate.java
+++ src/main/java/cids/grouptwo/Coordinate.java
@@ -13,8 +13,4 @@ public class Coordinate {
         Y = y;
     }
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> main
--- src/main/java/cids/grouptwo/Main.java
+++ src/main/java/cids/grouptwo/Main.java
@@ -1,5 +1,6 @@
 package cids.grouptwo;
 
+import java.io.IOException;
 import java.util.prefs.Preferences;
 
 import com.badlogic.gdx.Graphics;
@@ -47,6 +48,21 @@ public class Main {
             config.setFullscreenMode(dm);
         new Lwjgl3Application(new ChessGame(h), config);
         
+    }
+
+    public static void clear() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+            System.out.println("Clear failed :( " + e);
+        }  
     }
 
 }
